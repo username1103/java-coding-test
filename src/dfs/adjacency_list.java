989c5d5e61788134d70900bcc231fdaa31237360
myeongil <username1103@naver.com>
2022-05-23 23:33:51 +0900

    이코테 인접 행령과 인접 리스트

---
+++ src/dfs/adjacency_list.java
@@ -0,0 +1,43 @@
+package dfs;
+
+import java.util.ArrayList;
+
+public class adjacency_list {
+    static class Node {
+        int distance;
+        int index;
+
+        public Node(int index, int distance) {
+            this.index = index;
+            this.distance = distance;
+        }
+
+        public void show() {
+            System.out.println("index = " + index + ", distance = " + distance);
+        }
+    }
+
+    public static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 3; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        graph.get(0).add(new Node(1, 7));
+        graph.get(0).add(new Node(2, 5));
+
+        graph.get(1).add(new Node(0, 7));
+
+        graph.get(2).add(new Node(0, 5));
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < graph.get(i).size(); j++) {
+                graph.get(i).get(j).show();
+            }
+            System.out.println();
+        }
+
+
+    }
+}
