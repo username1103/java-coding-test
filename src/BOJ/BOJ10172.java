f651aa9aad3d81375468fff05fd6ced56e457659
myeongil <username1103@naver.com>
2022-06-07 18:18:56 +0900

    백준

---
+++ src/BOJ/BOJ10172.java
@@ -0,0 +1,17 @@
+package BOJ;
+/*
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+ */
+public class BOJ10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
