ce03031feee3f5401469b1ab795f5ef293cd611c
myeongil <username1103@naver.com>
2022-06-18 14:12:26 +0900

    BOJ 14888 연산자 끼워 넣기 풀이 추가

--- src/BOJ/BOJ14888.java
+++ src/BOJ/BOJ14888.java
@@ -47,6 +47,14 @@ public class BOJ14888 {
 
     static int[] operations;
 
+    static int min = (int) 10e9;
+    static int max = (int) 10e9 * -1;
+
+    static int plusCount;
+    static int minusCount;
+    static int mulCount;
+    static int divCount;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
@@ -57,59 +65,83 @@ public class BOJ14888 {
             values[i] = sc.nextInt();
         }
 
-
-        int plusCount = sc.nextInt();
-        for (int i = 0; i < plusCount; i++) {
-            operations[i] = '+';
-        }
-
-        int minusCount = sc.nextInt();
-        for (int i = plusCount; i <plusCount + minusCount ; i++) {
-            operations[i] = '-';
-        }
-
-        int mulCount = sc.nextInt();
-        for (int i = plusCount + minusCount; i < plusCount + minusCount + mulCount; i++) {
-            operations[i] = '*';
-        }
-
-        int divCount = sc.nextInt();
-        for (int i = plusCount+minusCount+mulCount; i <plusCount+minusCount+mulCount+divCount ; i++) {
-            operations[i] = '/';
-        }
-
-        Permutation perm = new Permutation(operations.length, operations.length);
-        perm.permutation(operations, 0);
+        plusCount = sc.nextInt();
+        minusCount = sc.nextInt();
+        mulCount = sc.nextInt();
+        divCount = sc.nextInt();
+//        for (int i = 0; i < plusCount; i++) {
+//            operations[i] = '+';
+//        }
+//        for (int i = plusCount; i <plusCount + minusCount ; i++) {
+//            operations[i] = '-';
+//        }
+//        for (int i = plusCount + minusCount; i < plusCount + minusCount + mulCount; i++) {
+//            operations[i] = '*';
+//        }
+//        for (int i = plusCount+minusCount+mulCount; i <plusCount+minusCount+mulCount+divCount ; i++) {
+//            operations[i] = '/';
+//        }
+//
+//        Permutation perm = new Permutation(operations.length, operations.length);
+//        perm.permutation(operations, 0);
 
 
-        int max = -1000000001;
-        int min = 1000000001;
-        for (int i = 0; i < perm.results.size(); i++) {
-            int[] result = perm.results.get(i);
-            int temp = values[0];
-            for (int j = 0; j < result.length; j++) {
-                int operation = result[j];
-                switch(operation){
-                    case '+':
-                        temp += values[j + 1];
-                        break;
-                    case '-':
-                        temp -= values[j + 1];
-                        break;
-                    case '*':
-                        temp *= values[j + 1];
-                        break;
-                    case '/':
-                        temp /= values[j + 1];
-                        break;
-                }
-            }
-            max = Math.max(max, temp);
-            min = Math.min(min, temp);
-        }
+//        for (int i = 0; i < perm.results.size(); i++) {
+//            int[] result = perm.results.get(i);
+//            int temp = values[0];
+//            for (int j = 0; j < result.length; j++) {
+//                int operation = result[j];
+//                switch(operation){
+//                    case '+':
+//                        temp += values[j + 1];
+//                        break;
+//                    case '-':
+//                        temp -= values[j + 1];
+//                        break;
+//                    case '*':
+//                        temp *= values[j + 1];
+//                        break;
+//                    case '/':
+//                        temp /= values[j + 1];
+//                        break;
+//                }
+//            }
+//            max = Math.max(max, temp);
+//            min = Math.min(min, temp);
+//        }
+
+        dfs(1, values[0]);
 
         System.out.println(max);
         System.out.println(min);
 
+    }
+
+    static void dfs(int i, int now) {
+        if (i == n) {
+            min = Math.min(min, now);
+            max = Math.max(max, now);
+        } else {
+            if (plusCount > 0) {
+                plusCount -= 1;
+                dfs(i + 1, now + values[i]);
+                plusCount += 1;
+            }
+            if (minusCount > 0) {
+                minusCount -= 1;
+                dfs(i + 1, now - values[i]);
+                minusCount += 1;
+            }
+            if (mulCount > 0) {
+                mulCount -= 1;
+                dfs(i + 1, now * values[i]);
+                mulCount += 1;
+            }
+            if (divCount > 0) {
+                divCount -= 1;
+                dfs(i + 1, now / values[i]);
+                divCount += 1;
+            }
+        }
     }
 }
