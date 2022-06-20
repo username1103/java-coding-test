7cafff45f281f126f4e82b41a16f76e9a51521ba
myeongil <username1103@naver.com>
2022-06-20 21:12:52 +0900

    BOJ 15655 N과 M(6)

---
+++ src/BOJ/BOJ15655.java
@@ -0,0 +1,55 @@
+package BOJ;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ15655 {
+
+    static boolean[] visited;
+    static int[] values;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        values = new int[n];
+        visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            values[i] = sc.nextInt();
+        }
+
+        Arrays.sort(values);
+
+        int[] result = new int[r];
+        process(values, n, r, 0,0, result);
+
+
+        System.out.println(sb);
+    }
+
+    public static void process(int[] values, int n, int r, int depth, int index, int[] result) {
+        if (depth == r) {
+            for (int i = 0; i < r; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+
+        for (int i = index; i < n; i++) {
+            if (visited[i] == false) {
+                visited[i] = true;
+                result[depth] = values[i];
+                process(values, n, r, depth + 1, i + 1, result);
+                visited[i] = false;
+            }
+
+        }
+    }
+}
+
+
