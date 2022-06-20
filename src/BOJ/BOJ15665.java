40ab84cccdd167c403f985c92d1244f4203b5744
myeongil <username1103@naver.com>
2022-06-20 21:48:31 +0900

    BOJ 15665 N과 M(11)

---
+++ src/BOJ/BOJ15665.java
@@ -0,0 +1,48 @@
+package BOJ;
+
+import java.util.*;
+
+public class BOJ15665 {
+
+    static boolean[] visited;
+    static int[] values;
+
+    static Set<String> s = new LinkedHashSet<>();
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        values = new int[n];
+        visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            values[i] = sc.nextInt();
+        }
+
+        Arrays.sort(values);
+
+        process(values, n, r, 0, "");
+
+
+        for (String a : s) {
+            System.out.println(a);
+        }
+
+    }
+
+    public static void process(int[] values, int n, int r, int depth, String result) {
+        if (depth == r) {
+            s.add(result);
+            return;
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            process(values, n, r, depth + 1, result + values[i] + " ");
+        }
+    }
+}
+
+
