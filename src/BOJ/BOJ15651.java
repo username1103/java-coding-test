33843325936f738888a74c5c426b37f9541844ce
myeongil <username1103@naver.com>
2022-06-20 20:42:59 +0900

    BOJ 15651 N과 M(3)

---
+++ src/BOJ/BOJ15651.java
@@ -0,0 +1,47 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ15651 {
+
+    static boolean[] visited;
+    static int[] values;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        values = new int[n];
+        visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            values[i] = i + 1;
+        }
+
+        int[] result = new int[r];
+        process(values, n, r, 0, result);
+
+
+        System.out.println(sb);
+    }
+
+    public static void process(int[] values, int n, int r, int depth, int[] result) {
+        if (depth == r) {
+            for (int i = 0; i < r; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            result[depth] = values[i];
+            process(values, n, r, depth + 1, result);
+
+        }
+    }
+}
+
+
