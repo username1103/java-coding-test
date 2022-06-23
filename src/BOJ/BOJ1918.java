5dce6f60c3ac8fc2f2b5d93937ecd599097bcdcc
myeongil <username1103@naver.com>
2022-06-23 21:27:54 +0900

    BOJ 1918 후위 표기식 설명

--- src/BOJ/BOJ1918.java
+++ src/BOJ/BOJ1918.java
@@ -2,6 +2,7 @@ package BOJ;
 
 import java.util.*;
 
+
 public class BOJ1918 {
     public static void main(String[] args) {
 
@@ -11,6 +12,13 @@ public class BOJ1918 {
 
         StringBuilder sb = new StringBuilder();
         Stack<Character> s = new Stack<>();
+
+        // 1. 문자는 그대로 출력한다.
+        // 2. (는 스택에 추가한다.
+        // 3. )가 나오면 스택에서 다음이 (나올 때 까지 또는 스택이 빌때까지 출력한다. (제거해준다.
+        // 4. +, -가 나오면 스택에 다음이 (가 나오거나 빌 때까지 출력한다. 이후 자기자신을 스택에 추가한다.
+        // 5. *, /가 나오면 스택에 비지 않고 * 또는 /가 다음이면 출력한다. 이후 자기자신을 스택에 추가한다.
+        // 6. 스택에 남은 전부를 출력한다.
 
         for (int i = 0; i < input.length(); i++) {
             char val = input.charAt(i);
